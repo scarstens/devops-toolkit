2bbe5d602bc041474c7e6826da9af720e16a1eb6
Seth Carstens <seth.carstens@gmail.com>
2018-01-26 01:14:26 -0700

    Create fix_aws_ec2_defaults.sh

---
+++ bash/fix_aws_ec2_defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Applying AWS UBUNTU 16 DEFAULT ADJUSTMENTS FOR WEB APPLICATIONS"
+echo "Fixing unable to resolve hosts when VPC doesn't allow DNS hostnames..."
+echo "127.0.0.1 $(hostname)" | sudo tee -a /etc/hosts 2>&1 /dev/null
+echo "Making ubuntu's primary group www-data to match php/nginx group for easier permissioning..."
+#sudo usermod -g www-data ubuntu
+exec sg www-data newgrp `id -gn`
