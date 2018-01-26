d443cfb2e012d0d45b8d998fdec8410bf1ba28ba
Seth Carstens <seth.carstens@gmail.com>
2018-01-26 14:31:51 -0700

    Update fix_aws_ec2_defaults.sh

--- bash/fix_aws_ec2_defaults.sh
+++ bash/fix_aws_ec2_defaults.sh
@@ -4,3 +4,4 @@ echo "Applying AWS UBUNTU 16 DEFAULT ADJUSTMENTS FOR WEB APPLICATIONS"
 # echo "127.0.0.1 $(hostname)" | sudo tee -a /etc/hosts 2>&1 /dev/null
 echo "Making ubuntu's primary group www-data to match php/nginx group for easier permissioning..."
 sudo usermod -g www-data ubuntu
+echo "Please exit and log back in to fix user permissions..."
