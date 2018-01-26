3796093225ff92f80f1f60fe4d2eab46c462c59f
Seth Carstens <seth.carstens@gmail.com>
2018-01-26 16:50:23 -0700

    Create create_user.sh

---
+++ bash/create_user.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo 'Enter username:'
+read un
+
+if id -u $un >/dev/null 2>&1; then
+    echo "user exists: $un"
+    exit;
+fi
+
+# Assumed Else
+sudo adduser --disabled-password --gecos "" $un
+echo "added user: $un"
+echo "paste public key:"
+read key
+sudo su - $un <<USER_COMMANDS
+    mkdir .ssh
+    chmod 700 .ssh
+    touch .ssh/authorized_keys
+    chmod 600 .ssh/authorized_keys
+    echo "$key" >> .ssh/authorized_keys
+USER_COMMANDS
+
+#bash /home/ec2-user/build_developer_auth_keys.sh
+echo "key added, user successfully created -- $un:$key"
+sudo usermod -aG sudo $un
+sudo usermod -g www-data $un
